c865ae551836242427a021c68b8080257b428f2d
NicolasBischof <33013532+NicolasBischof@users.noreply.github.com>
2018-07-07 16:08:15 -0400

    Update Tetris.py

--- Tetris.py
+++ Tetris.py
@@ -10,7 +10,7 @@ def coord_in_px(coord):
     return (coord[0] * BLOCK_SIZE,
             coord[1] * BLOCK_SIZE)
 
-
+DRAW_SPOTLIGHT = True # the spotlight is a guide the width of the player that extends to where it would fall.
 STAGE_WIDTH = 9 # stage is where the game takes place.
 STAGE_HEIGHT = 17
 STAGE_MARGIN = (1,1)
@@ -26,7 +26,7 @@ STAGE_BOTTOM_RIGHT = (STAGE_MARGIN[0] + STAGE_WIDTH, STAGE_MARGIN[1] + STAGE_HEI
 
 BORDER_COLOR = (160,160,160)
 BORDER_WIDTH = 1 #the borders surrounding areas
-DRAW_SPOTLIGHT = True # the spotlight is a guide the width of the player that extends to where it would fall.
+
 SPOTLIGHT_COLOR = (15,15,15)
 TITLE_COLOR = (0, 64, 128)
 TEXT_COLOR = (255,255,255)
@@ -440,6 +440,4 @@ class Game:
 
 
 if __name__ == '__main__':
-
-    game = Game()
-    game.run()
+    Game().run()
