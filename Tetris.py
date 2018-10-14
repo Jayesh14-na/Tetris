11f0da49be71369d0c7d69f0da8b5d6e2b993660
NicolasBischof <33013532+NicolasBischof@users.noreply.github.com>
2018-10-14 14:18:29 -0300

    Update Tetris.py

--- Tetris.py
+++ Tetris.py
@@ -150,7 +150,7 @@ class Piece:
 
 
 
-class GameCore:
+class Game:
     """Manages the interaction between the player and movement, margins, lower pieces, states, score, next piece and level."""
     def __init__(self, piece_name = None):
         self.playing = True
@@ -161,6 +161,11 @@ class GameCore:
         self.next_piece = Piece()
         self.score = Score()
         self.bottom_pieces = BottomPieces(self)
+        self.draw = DrawGame(self)
+        self.event_handler = EventHandler(self)
+    
+    def run(self):
+        self.event_handler.run()
 
     def set_paused(self, pause = None):
         self.paused = pause if pause != None else not self.paused
@@ -317,40 +322,73 @@ class Clock:
 
 
 
-class Game:
-    "Draws stuff and handles events"
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode(coord_in_px(SCREEN_SIZE))
-        pygame.display.set_caption(NAME_OF_THE_GAME)
+class EventHandler:
+    def __init__(self, game):
         pygame.key.set_repeat(1,160)
-        self.core = GameCore()
+        self.game = game
         self.running = True # is the application running
-        self.clock = Clock(self.core.score)
-        self.font = pygame.font.SysFont("Arial", 22)
+        self.clock = Clock(self.game.score)
 
     def run(self):
         while self.running:
             self.clock.tick()
             self.handle_events()
-            self.draw_game()
+            self.game.draw.draw_game()
 
     def tick(self):
-        self.core.move_player( (0,1) )
+        self.game.move_player( (0,1) )
+
+    def handle_events(self):
+        keys_pressed = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+            
+            if event.type == KEYDOWN:
+                if event.key in (K_PAUSE, K_RETURN, K_p):
+                    if not self.game.game_over:
+                        self.game.set_paused()
+                    else:
+                        self.__init__()
+                
+            if self.game.playing:
+                if event.type == KEYDOWN:
+                    if event.key == K_UP:
+                        self.game.rotate_player()
+                    elif event.key == K_DOWN:
+                        self.game.move_player( (0,1) )
+                    elif event.key == K_RIGHT:
+                        self.game.move_player( (1,0) )
+                    elif event.key == K_LEFT:
+                        self.game.move_player( (-1,0) )
+                    elif event.key == K_SPACE:
+                        if keys_pressed[K_RIGHT]:
+                            self.game.move_piece_to_limit((1,0))
+                        elif keys_pressed[K_LEFT]:
+                            self.game.move_piece_to_limit((-1,0))
+                        else:
+                            self.game.move_piece_to_limit((0,1))
+                elif event.type == LOWER_PIECE_EVENT_ID:
+                    self.game.move_player( (0,1) )
+
+
+
+class DrawGame:
+    def __init__(self, game):
+        pygame.init()
+        self.screen = pygame.display.set_mode(coord_in_px(SCREEN_SIZE))
+        pygame.display.set_caption(NAME_OF_THE_GAME)
+        self.game = game
+        self.font = pygame.font.SysFont("Arial", 22)
+
+
 
     def draw_game(self):
         self.screen.fill((0, 0, 0))
-        if self.core.game_over:
-            self.draw_game_over()
-        if self.core.paused:
-            self.draw_pause()
+        self.draw_text()
         if DRAW_SPOTLIGHT:
             self.draw_spotlight()
         self.draw_pieces()
-        self.draw_number_of_lines()
-        self.draw_score()
-        self.draw_level_number()
-        self.draw_next_piece()
         self.draw_borders()
         pygame.display.flip()
 
@@ -360,27 +398,37 @@ class Game:
         txt_pos[0] -= text.get_width()//2
         self.screen.blit(text, txt_pos)    
 
-    def draw_number_of_lines(self):
+    def draw_txt_number_of_lines(self):
         self.render_text("LINES", (BOARD_CENTER_X, 1.5), TITLE_COLOR)
-        self.render_text(str(self.core.score.lines), (BOARD_CENTER_X, 2.5), TEXT_COLOR)
+        self.render_text(str(self.game.score.lines), (BOARD_CENTER_X, 2.5), TEXT_COLOR)
 
-    def draw_level_number(self):
+    def draw_txt_level_number(self):
         self.render_text("LEVEL", (BOARD_CENTER_X, 4.5), TITLE_COLOR)
-        self.render_text(str(self.core.score.level), (BOARD_CENTER_X, 5.5), TEXT_COLOR)
+        self.render_text(str(self.game.score.level), (BOARD_CENTER_X, 5.5), TEXT_COLOR)
 
-    def draw_score(self):
+    def draw_txt_score(self):
         self.render_text("SCORE", (BOARD_CENTER_X, 7.5), TITLE_COLOR)
-        self.render_text(str(self.core.score.score), (BOARD_CENTER_X, 8.5), TEXT_COLOR)
+        self.render_text(str(self.game.score.score), (BOARD_CENTER_X, 8.5), TEXT_COLOR)
  
-    def draw_game_over(self):
+    def draw_txt_game_over(self):
         self.render_text("GAME OVER", (BOARD_CENTER_X, 10.5), GAME_OVER_TEXT_COLOR)
 
-    def draw_pause(self):
+    def draw_txt_pause(self):
         self.render_text("PAUSE", (BOARD_CENTER_X, 10.5), PAUSE_TEXT_COLOR)
 
     
-    def draw_next_piece(self):
+    def draw_txt_next_piece(self):
         self.render_text("NEXT PIECE", (BOARD_CENTER_X, 12.5), TITLE_COLOR)
+    
+    def draw_text(self):
+        if self.game.game_over:
+            self.draw_txt_game_over()
+        if self.game.paused:
+            self.draw_txt_pause()
+        self.draw_txt_number_of_lines()
+        self.draw_txt_score()
+        self.draw_txt_level_number()
+        self.draw_txt_next_piece()
         
 
     def draw_piece(self, piece):
@@ -390,19 +438,19 @@ class Game:
             pygame.draw.rect(self.screen, piece.color, Rect(pixel_pos, size_rect))
 
     def draw_spotlight(self):
-        top_left_x = coord_in_px(self.core.player.blocks[0])[0]
+        top_left_x = coord_in_px(self.game.player.blocks[0])[0]
         top_left_y = coord_in_px(STAGE_TOP_LEFT)[1]
         top_left = (top_left_x, top_left_y)
-        x_for_bottom_x = self.core.player.blocks[-1][0] - self.core.player.blocks[0][0]
+        x_for_bottom_x = self.game.player.blocks[-1][0] - self.game.player.blocks[0][0]
         bottom_right_x = coord_in_px((x_for_bottom_x + STAGE_MARGIN[0], 1))[0]
         bottom_right_y = coord_in_px(STAGE_BOTTOM_RIGHT)[1]
         bottom_right = (bottom_right_x, bottom_right_y)
         pygame.draw.rect(self.screen, SPOTLIGHT_COLOR, Rect(top_left, bottom_right))
 
     def draw_pieces(self):
-        self.draw_piece(self.core.player)
-        self.draw_piece(self.core.next_piece)
-        for piece in self.core.bottom_pieces:
+        self.draw_piece(self.game.player)
+        self.draw_piece(self.game.next_piece)
+        for piece in self.game.bottom_pieces:
             self.draw_piece(piece)
 
     def draw_borders(self):
@@ -413,38 +461,6 @@ class Game:
         right_area_rect = Rect(coord_in_px(right_area_top_left), coord_in_px(right_area_bottom_right))
         pygame.draw.rect(self.screen, BORDER_COLOR, right_area_rect, BORDER_WIDTH)
 
-    def handle_events(self):
-        keys_pressed = pygame.key.get_pressed()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.running = False
-            
-            if event.type == KEYDOWN:
-                if event.key in (K_PAUSE, K_RETURN, K_p):
-                    if not self.core.game_over:
-                        self.core.set_paused()
-                    else:
-                        self.__init__()
-                
-            if self.core.playing:
-                if event.type == KEYDOWN:
-                    if event.key == K_UP:
-                        self.core.rotate_player()
-                    elif event.key == K_DOWN:
-                        self.core.move_player( (0,1) )
-                    elif event.key == K_RIGHT:
-                        self.core.move_player( (1,0) )
-                    elif event.key == K_LEFT:
-                        self.core.move_player( (-1,0) )
-                    elif event.key == K_SPACE:
-                        if keys_pressed[K_RIGHT]:
-                            self.core.move_piece_to_limit((1,0))
-                        elif keys_pressed[K_LEFT]:
-                            self.core.move_piece_to_limit((-1,0))
-                        else:
-                            self.core.move_piece_to_limit((0,1))
-                elif event.type == LOWER_PIECE_EVENT_ID:
-                    self.core.move_player( (0,1) )
 
 
 
