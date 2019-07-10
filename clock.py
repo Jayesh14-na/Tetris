5966e58619ac014543b28c9b9a028ea72733e905
NicolasBischof <33013532+NicolasBischof@users.noreply.github.com>
2019-07-10 18:56:03 -0400

    Update clock.py

--- clock.py
+++ clock.py
@@ -2,22 +2,17 @@ import pygame
 from constants import *
 
 class Clock:
-    def __init__(self, score):
-        self.score = score
+    def __init__(self, game):
+        self.game = game
         self.clock = pygame.time.Clock()
         self.fps = FPS
-        self.lower_piece_data = {n:int(48 - 2.4 * n) for n in range(21)}
         self.time_accumulated_to_lower_piece = 0
 
-    lower_piece_every = property(lambda self: self.lower_piece_data[self.score.level] or 2)
+    lower_piece_every = property(lambda self: self.game.lower_piece_data[self.game.score.level] or 2) # TODO: explain
 
     def tick(self):
         elapsed_time = self.clock.tick(self.fps)
-        self.process_lower_piece(elapsed_time)
-
-    def process_lower_piece(self, elapsed_time):
         self.time_accumulated_to_lower_piece += elapsed_time
         if self.time_accumulated_to_lower_piece >= self.lower_piece_every * 1000//FPS:
+            self.game.trigger_event_lower_piece()
             self.time_accumulated_to_lower_piece = 0
-            event_lower_piece = pygame.event.Event(LOWER_PIECE_EVENT_ID)
-            pygame.event.post(event_lower_piece)
